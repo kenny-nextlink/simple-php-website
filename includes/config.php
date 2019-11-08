75a2189c54d78adf9df27da27ea5d501baa7fb2e
unknown <kenny.tsai@nextlink.com.tw>
2019-11-08 10:59:50 +0800

    Update

--- includes/config.php
+++ includes/config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple PHP Website',
+        'name' => 'Kenny\'s Simple PHP Website',
         'site_url' => '',
         'pretty_uri' => true,
         'nav_menu' => [
